035ac3c08d4cb192165669482edb7e3fc72d3856
Faheim Arslan <faheimarslan@gmail.com>
2023-10-16 12:42:32 +0100

    added projects and gsap

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,10 @@
-import * as React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import styles from "../styles/ProjectList.module.css";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
   const projectList = [
@@ -11,7 +17,42 @@ const Projects = () => {
     "Fashion Generator using GANs",
   ];
 
-  const bgColors = ["yellow", "blue", "red", "green", "teal", "cyan", "fuchsia"];
+  const bgColors = [
+    "yellow",
+    "blue",
+    "red",
+    "green",
+    "teal",
+    "cyan",
+    "fuchsia",
+  ];
+
+  useEffect(() => {
+    // Calculate the total width of the project items
+    const projectListContainer = document.querySelector(".main-project-panel");
+    const projectItems = document.querySelectorAll(".project-panel");
+    const totalWidth = Array.from(projectItems).reduce(
+      (acc, item) => acc + item.clientWidth,
+      0
+    );
+
+    // Create a GSAP timeline for the scrolling animation
+    gsap.to(projectItems, {
+      ease: "none", // Adjust easing as needed
+      scrollTrigger: {
+        trigger: ".projects",
+        pin: "true",
+        start: "50% bottom",
+        scrub: 1,
+        markers: "true",
+        anticipatePin: 1,
+      },
+    });
+
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
   return (
     <section className="projects min-h-screen">
@@ -20,14 +61,25 @@ const Projects = () => {
       </div>
       <br />
 
-      <div className="project-list flex">
-        {projectList.map((child, index) => (
-          <div className="mx-24 mt-2" key={index}>
-            <div className={`flex items-center rounded-[4rem] px-3 py-1 h-128 w-168 text-md font-roboto-light leading-5 bg-${bgColors[index]}-200`}>
-              {child}
+      <div
+        className="main-project-panel p-48 flex"
+        style={{ width: "100%", overflowX: "auto" }}
+      >
+        <div></div>
+        <div className="projects-container flex">
+          {projectList.map((child, index) => (
+            <div className="project-panel mx-24 mt-2" key={index}>
+              <div
+                className={`panel flex items-center rounded-[4rem] shadow-xl px-3 py-1 h-128 w-168 text-md font-roboto-light leading-5`}
+                style={{
+                  backgroundColor: `var(--saturated-${bgColors[index]})`,
+                }}
+              >
+                {child}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
